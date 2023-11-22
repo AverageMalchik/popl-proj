d1f027f53139dd75d95741f7a7dab5c24c70b542
AverageMalchik <muhdjibran@hotmail.com>
2023-11-22 22:22:16 +0530

    New: Added count option

--- src/grep.rs
+++ src/grep.rs
@@ -5,34 +5,48 @@ use crate::grep_command_builder::GrepOptions;
 use r3bl_rs_utils::{style_primary, style_prompt};
 
 pub fn grep(options: GrepOptions) -> Result<(), Box<dyn Error>> {
-  println!(
-    "{}: search for '{}' in '{}' w/ {}",
-    style_prompt("DEBUG"),
-    options.search,
-    options.file_path,
-    match options.case_sensitive {
-      true => "case sensitive",
-      false => "case insensitive",
-    }
-  );
-
-  let content = fs::read_to_string(options.file_path)?;
-  let filtered_content = content
-    .lines()
-    .filter(|line| {
-      if options.case_sensitive {
-        line.contains(&options.search)
-      } else {
-        line.to_lowercase().contains(&options.search.to_lowercase())
+    println!(
+      "{}: search for '{}' in '{}' w/ {} {}",
+      style_prompt("DEBUG"),
+      options.search,
+      options.file_path,      
+      match options.case_sensitive {
+        true => "case sensitive",
+        false => "case insensitive",
+      },
+      match options.count {
+          true => "only count",
+          false => "",
       }
-    })
-    .map(|line| {
-      let from = &options.search;
-      let to = format!("{}", style_primary(&options.search));
-      line.replace(from, &to)
-    })
-    .collect::<Vec<String>>();
-  println!("{}", filtered_content.join("\n"));
+    );
 
-  Ok(())
+    let mut counter = 0;
+
+    let content = fs::read_to_string(options.file_path)?;
+    let filtered_content = content
+        .lines()
+        .filter(|line| {
+            let contains = if options.case_sensitive {
+                line.contains(&options.search)
+            } else {
+                line.to_lowercase().contains(&options.search.to_lowercase())
+            };
+            if contains {
+                counter += 1;
+            }
+            contains
+        })
+        .map(|line| {
+            let from = &options.search;
+            let to = format!("{}", style_primary(&options.search));
+            line.replace(from, &to)
+        })
+        .collect::<Vec<String>>();
+    if options.count {
+        println!("{}", counter);
+    } else {
+        println!("{}", filtered_content.join("\n"));
+    }
+
+    Ok(())
 }
--- src/grep_command_builder.rs
+++ src/grep_command_builder.rs
@@ -3,6 +3,7 @@ pub struct GrepOptions {
   pub search: String,
   pub file_path: String,
   pub case_sensitive: bool,
+  pub count: bool,
 }
 
 const REQUIRED_ARGS_COUNT: usize = 3;
@@ -32,61 +33,9 @@ impl GrepOptionsBuilder {
         None => String::new(),
       },
       case_sensitive: args.next().is_some(), // If 3rd arg exists, then true.
+      count: args.next().is_some(),
     };
 
     Ok(options)
-  }
-}
-
-#[test]
-fn test_can_build_options_with_3_args() {
-  let options = GrepOptionsBuilder::parse(vec![
-    String::from("program"),
-    String::from("search-string"),
-    String::from("path-to-file"),
-    String::from("case-sensitive"),
-  ]);
-  match options {
-    Ok(options) => {
-      assert_eq!(options.search, "search-string");
-      assert_eq!(options.file_path, "path-to-file");
-      assert_eq!(options.case_sensitive, true);
-    }
-    Err(error) => panic!("{}", error),
-  }
-}
-
-#[test]
-fn test_can_build_options_with_2_args() {
-  let options = GrepOptionsBuilder::parse(vec![
-    String::from("program"),
-    String::from("search-string"),
-    String::from("path-to-file"),
-  ]);
-  match options {
-    Ok(options) => {
-      assert_eq!(options.search, "search-string");
-      assert_eq!(options.file_path, "path-to-file");
-      assert_eq!(options.case_sensitive, false);
-    }
-    Err(error) => panic!("{}", error),
-  }
-}
-
-#[test]
-#[should_panic]
-fn test_can_not_build_options_with_1_args() {
-  let options = GrepOptionsBuilder::parse(vec![String::from("search-string")]);
-  if let Err(error) = options {
-    panic!("{}", error)
-  }
-}
-
-#[test]
-#[should_panic]
-fn test_can_not_build_options_with_0_args() {
-  let options = GrepOptionsBuilder::parse(vec![]);
-  if let Err(error) = options {
-    panic!("{}", error)
   }
 }
